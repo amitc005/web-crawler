10d081b9078c52719a5c2918866073b1c1046fe0
Amit Chavhan <achavahan@texadasoftware.com>
2021-10-25 16:33:03 -0400

    Added recusive logic and Cli framework
    - Now main method has a logic which fetch the urls recursively; first excutes the home page, collect the urls in home page, display the URLs and create process the URLs sequentially one by one
    - And also installed click package for Cli handling.

--- web_crawler.py
+++ web_crawler.py
@@ -1,14 +1,28 @@
 import requests
+import click
 from bs4 import BeautifulSoup
 
-URL = "http://www.rescale.com"
 
-
-def main():
-    res = requests.get(URL)
+def fetch_urls(url):
+    click.echo("Fetching URLs from {}".format(url))
+    res = requests.get(url)
     soup = BeautifulSoup(res.content, "html.parser")
-    for url in soup.find_all("a", href=lambda x: x and x.startswith("http")):
-        print(url.get("href"))
+    return soup.find_all("a", href=lambda x: x and x.startswith("http"))
+
+
+@click.command()
+@click.option("--url", required=True, help="Home Page URL")
+def main(url):
+    urls_to_fetch = [url]
+    while True:
+        res_urls = set()
+
+        for url in urls_to_fetch:
+            for url in fetch_urls(url):
+                click.echo(f"\t{url.get('href')}")
+                res_urls.add(url.get("href"))
+
+        urls_to_fetch = res_urls
 
 
 if __name__ == "__main__":
