ff30ea3dadb112e737b6f3e0cab984cb7e7aa883
Amit Chavhan <achavahan@texadasoftware.com>
2021-10-25 17:08:38 -0400

    Added filter logic for response content.
    - Added logic to accept only 200 success status code and text content format only for html

--- tests/test_fetch_urls.py
+++ tests/test_fetch_urls.py
@@ -1,14 +1,20 @@
 import unittest
 from unittest.mock import patch
 from web_crawler import fetch_urls
+from http import HTTPStatus
 
 URL = "https://test.com"
 
 
 class TestFetchURls(unittest.TestCase):
+    HTML_MIME_TYPE = "text/html"
+
     @patch("web_crawler.requests.get")
     def test_fetch_urls(self, mock_get):
         expected_urls = ["https://test1.com", "https://test2.com"]
+        mock_get.return_value.status_code = HTTPStatus.OK
+        mock_get.return_value.headers = {"content-type": self.HTML_MIME_TYPE}
+
         html_content = (
             f'<a href="{expected_urls[0]}"></br></a><a href="{expected_urls[1]}"></a>'
         )
@@ -18,3 +24,19 @@ class TestFetchURls(unittest.TestCase):
         self.assertEqual(len(urls), 2)
         urls = [url.get("href") for url in urls]
         self.assertEqual(urls, expected_urls)
+
+    @patch("web_crawler.requests.get")
+    def test_fetch_urls_with_unsuccess_response_code(self, mock_get):
+        mock_get.return_value.status_code = 404
+        mock_get.return_value.headers = {"content-type": self.HTML_MIME_TYPE}
+        urls = fetch_urls(URL)
+        self.assertTrue(isinstance(urls, list))
+        self.assertEqual(len(urls), 0)
+
+    @patch("web_crawler.requests.get")
+    def test_fetch_urls_with_unknown_mime_type(self, mock_get):
+        mock_get.return_value.status_code = 404
+        mock_get.return_value.headers = {"content-type": "unknown"}
+        urls = fetch_urls(URL)
+        self.assertTrue(isinstance(urls, list))
+        self.assertEqual(len(urls), 0)
--- web_crawler.py
+++ web_crawler.py
@@ -1,10 +1,17 @@
 import requests
 import click
 from bs4 import BeautifulSoup
+from http import HTTPStatus
 
 
 def fetch_urls(url):
     res = requests.get(url)
+    if (
+        res.status_code != HTTPStatus.OK
+        or res.headers.get("content-type") != "text/html"
+    ):
+        return []
+
     soup = BeautifulSoup(res.content, "html.parser")
     return soup.find_all("a", href=lambda x: x and x.startswith("http"))
 
@@ -15,10 +22,13 @@ def main(url):
     urls_to_fetch = [url]
     while True:
         res_urls = set()
+        for req_url in urls_to_fetch:
+            click.echo("Fetching URLs from {}".format(req_url))
+            fetched_urls = fetch_urls(req_url)
+            if not fetched_urls:
+                click.echo("{} is could be unsupported URL or empty".format(req_url))
 
-        for url in urls_to_fetch:
-            for url in fetch_urls(url):
-                click.echo("Fetching URLs from {}".format(url))
+            for url in fetched_urls:
                 click.echo(f"\t{url.get('href')}")
                 res_urls.add(url.get("href"))
 
